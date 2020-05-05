3a140ad5ad6c533faec36f2983a9a500c7db31df
amrukwa <a.mrukwa00@gmail.com>
2020-05-05 11:31:07 +0200

    Add checking filling the data

--- test/test.cpp
+++ test/test.cpp
@@ -18,13 +18,37 @@ namespace test
 			Assert::AreEqual(some_vector.n_features, other_vector.n_features);
 		}
 
-		TEST_METHOD(Test_values_are_assigned)
+		TEST_METHOD(Test_dimensions_are_assigned)
 		{
 			double* data = (double*)malloc(20*sizeof(double));
 			vectors some_vector(2, 10, data);
 			Assert::AreEqual(some_vector.n_features, 10);
 			Assert::AreEqual(some_vector.n_samples, 2);
-			Assert::AreEqual(some_vector.coords, data);
+		}
+
+		void check_matrix_filling(vectors* some_vector, double* data, int i, int j)
+		{
+			Assert::AreEqual(some_vector->coords[i * 3 + j], data[i * 3 + j]);
+		}
+
+		TEST_METHOD(Test_matrix_is_filled)
+		{
+			double* data = (double*)malloc(6 * sizeof(double));
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					data[i * 3 + j] = (i * 3) + j;
+				}
+			}
+			vectors some_vector(2, 3, data);
+			for (int i = 0; i < 2; i++)
+			{
+				for (int j = 0; j < 3; j++)
+				{
+					check_matrix_filling(&some_vector, data, i, j);
+				}
+			}
 		}
 	};
 }
