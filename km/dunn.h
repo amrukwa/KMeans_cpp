923e662588e704b4dab4a853ccdced4b90ede63c
amrukwa <a.mrukwa00@gmail.com>
2020-06-09 09:17:28 +0200

    Designate inter and intra functions

--- km/dunn.h
+++ km/dunn.h
@@ -32,6 +32,30 @@ double inter_avg(kmeans estim, vectors data)
 	return min_avg;
 }
 
+double inter_distance(kmeans estim, vectors data, inter_ metric = inter_::centroid)
+{
+	double inter = 0;
+	switch (metric)
+	{
+	case inter_::centroid:
+		inter = inter_centroid(estim);
+		break;
+	case inter_::closest:
+		inter = inter_closest(estim, data);
+		break;
+	case inter_::furthest:
+		inter = inter_furthest(estim, data);
+		break;
+	case inter_::avg:
+		inter = inter_avg(estim, data);
+		break;
+	default:
+		std::cout << "Invalid Selection for Dunn Index\n";
+		exit(1);
+	}
+	return inter;
+}
+
 double intra_closest(vectors labels, vectors data)
 {
 	double max_closest = 0;
@@ -53,47 +77,35 @@ double intra_avg(kmeans estim, vectors data)
 	return max_avg;
 }
 
+double intra_distance(kmeans estim, vectors data, intra_ metric = intra_::avg)
+{
+	double intra = 0;
+	switch (metric)
+	{
+	case intra_::closest:
+		intra = intra_closest(estim.labels, data);
+		break;
+	case intra_::furthest:
+		intra = intra_furthest(estim.labels, data);
+		break;
+	case intra_::avg:
+		intra = intra_avg(estim, data);
+		break;
+	default:
+		std::cout << "Invalid Selection for Dunn Index\n";
+		exit(1);
+	}
+	return intra;
+}
+
 double dunn_index(kmeans *estim, vectors data, inter_ metric1 = inter_::centroid, intra_ metric2 = intra_::avg)
 {
-	double inter = 0;
-	double intra = 0;
 	if (estim->centroids.n_samples == 1)
 	{
 		estim->fit(data);
 	}
-	switch (metric1)
-	{
-	case inter_::centroid:
-		inter = inter_centroid(*estim);
-		break;
-	case inter_::closest:
-		inter = inter_closest(*estim, data);
-		break;
-	case inter_::furthest:
-		inter = inter_furthest(*estim, data);
-		break;
-	case inter_::avg:
-		inter = inter_avg(*estim, data);
-		break;
-	default:
-		std::cout << "Invalid Selection for Dunn Index\n";
-		exit(1);
-	}
-	switch (metric2)
-	{
-	case intra_::closest:
-		intra = intra_closest(estim->labels, data);
-		break;
-	case intra_::furthest:
-		intra = intra_furthest(estim->labels, data);
-		break;
-	case intra_::avg:
-		intra = intra_avg(*estim, data);
-		break;
-	default:
-		std::cout << "Invalid Selection for Dunn Index\n";
-		exit(1);
-	}
+	double inter = inter_distance(*estim, data, metric1);
+	double intra = intra_distance(*estim, data, metric2);
 	return inter/intra;
 }
 
@@ -130,7 +142,6 @@ public:
 	{
 		for (int i = min_clusters; i < max_clusters; i++)
 		{
-
 		}
 	}
 };
