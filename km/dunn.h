bfe738acb982c05e1001ad6cb5e5e273fb2bedac
amrukwa <a.mrukwa00@gmail.com>
2020-06-09 07:46:04 +0200

    Remove unneccesary variables

--- km/dunn.h
+++ km/dunn.h
@@ -1,20 +1,24 @@
 #pragma once
 # include "kmeans.h"
 
+double dunn_index(kmeans estim, vectors data)
+{
+	double dunn = 0;
+	return dunn;
+}
 
 class DunnSearch 
 {
 public:
 	kmeans estimator;
-	vectors data;
 	std::string intra;
 	std::string inter;
 	int max_clusters;
 	int min_clusters;
 
-	DunnSearch(kmeans est, vectors x, std::string inter_d = "centroid", std::string intra_d = "avg", int min = 2, int max = 10):
+
+	DunnSearch(kmeans est, std::string inter_d = "centroid", std::string intra_d = "avg", int min = 2, int max = 10):
 		estimator(est),
-		data(x),
 		inter{inter_d},
 		intra{intra_d},
 		max_clusters{max},
@@ -26,10 +30,17 @@ public:
 
 	DunnSearch(const DunnSearch& estim):
 		estimator(estim.estimator),
-		data(estim.data),
 		intra{ estim.intra },
 		inter{ estim.inter },
 		max_clusters{ estim.max_clusters },
 		min_clusters{ estim.min_clusters }
 	{}
+
+	void fit(vectors data)
+	{
+		for (int i = min_clusters; i < max_clusters; i++)
+		{
+
+		}
+	}
 };
