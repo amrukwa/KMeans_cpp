5eb20413232c6177b0f580296032519e345c7757
amrukwa <a.mrukwa00@gmail.com>
2020-06-13 22:47:55 +0200

    Add Silhouette class

---
+++ km/silhouette.h
@@ -0,0 +1,51 @@
+#pragma once
+# include "kmeans.h"
+
+class SilhouetteSearch
+{
+public:
+	kmeans estimator;
+	int max_clusters = 10;
+	int min_clusters = 2;
+	double coefficient = 0.0;
+
+	SilhouetteSearch(kmeans est, int min = 2, int max = 10) :
+		estimator(est),
+		max_clusters{ max },
+		min_clusters{ min }
+	{}
+
+	~SilhouetteSearch()
+	{}
+
+	SilhouetteSearch(const SilhouetteSearch& estim) :
+		estimator(estim.estimator),
+		max_clusters{ estim.max_clusters },
+		min_clusters{ estim.min_clusters },
+		coefficient{ estim.coefficient }
+	{}
+
+	double single_coefficient(kmeans* est, vectors data, int clusters_n)
+	{
+		if (est->n_clusters != clusters_n)
+			est->n_clusters = clusters_n;
+		est->fit(data);
+		return 0;
+	}
+
+	void fit(vectors data)
+	{
+		double coef;
+		kmeans temp;
+		for (int i = min_clusters; i <= max_clusters; i++)
+		{
+			temp = estimator;
+			coef = single_coefficient(&temp, data, i);
+			if (coef > coefficient)
+			{
+				estimator = temp;
+				coefficient = coef;
+			}
+		}
+	}
+};
